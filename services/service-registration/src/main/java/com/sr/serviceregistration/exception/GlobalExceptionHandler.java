ee586360f60dab37228041ef0cb78feb24ae209f
vuhoan159357@gmail.com <vuhoan159357@gmail.com>
2025-05-12 01:39:53 +0700

    Registrations Service Added

---
+++ services/service-registration/src/main/java/com/sr/serviceregistration/exception/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.sr.serviceregistration.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+
+    Map<String, String> errors = new HashMap<>();
+    ex.getBindingResult().getAllErrors().forEach((error) -> {
+        errors.put(error.getCode(), error.getDefaultMessage());
+    });
+
+    return ResponseEntity.badRequest().body(errors);}
+
+
+    @ExceptionHandler(RegistrationNotFoundException.class)
+    public ResponseEntity<Map<String,String>> handleRegistrationNotFoundException(Exception ex) {
+        Map<String, String> errors = new HashMap<>();
+        errors.put("error", "Registration not found");
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
---
+++ services/service-registration/src/main/java/com/sr/serviceregistration/model/Registration.java
@@ -0,0 +1,50 @@
+package com.sr.serviceregistration.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
+
+@Entity
+public class Registration {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotNull
+    private String id_student;
+
+    @NotNull
+    private String id_course;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getId_student() {
+        return id_student;
+    }
+
+    public void setId_student(String id_student) {
+        this.id_student = id_student;
+    }
+
+    public String getId_course() {
+        return id_course;
+    }
+
+    public void setId_course(String id_course) {
+        this.id_course = id_course;
+    }
+
+
+}
