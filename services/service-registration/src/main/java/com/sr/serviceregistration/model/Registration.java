49c432261f8b77cf2d539ccb736dc4c35ad73d50
vuhoan159357@gmail.com <vuhoan159357@gmail.com>
2025-05-19 18:51:14 +0700

    Implement ApiGateWay: Nginx, Service Registry: Eureka

--- services/service-registration/src/main/java/com/sr/serviceregistration/model/Registration.java
+++ services/service-registration/src/main/java/com/sr/serviceregistration/model/Registration.java
@@ -1,10 +1,7 @@
 package com.sr.serviceregistration.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.UUID;
